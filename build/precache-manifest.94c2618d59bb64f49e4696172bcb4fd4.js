05e620a61fc0c39c2312da9309a67ef0199c6148
shigekof <100000100+shigekof@users.noreply.github.com>
2022-02-21 20:05:13 +1300

    Add files via upload

---
+++ build/precache-manifest.94c2618d59bb64f49e4696172bcb4fd4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b7c5893d71e83a2a5d5941e05883f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4315941b2b6170ad2d09",
+    "url": "/static/css/3.e95bb23f.chunk.css"
+  },
+  {
+    "revision": "bdd15377e0a952d4f696",
+    "url": "/static/css/4.b13a96d7.chunk.css"
+  },
+  {
+    "revision": "75e7f9091437f3a7fe07",
+    "url": "/static/css/5.19738895.chunk.css"
+  },
+  {
+    "revision": "0b3641182c6e72eee7fb",
+    "url": "/static/css/main.7590c290.chunk.css"
+  },
+  {
+    "revision": "6b6a89d51b1fad8f3362",
+    "url": "/static/js/2.e7fc8bc1.chunk.js"
+  },
+  {
+    "revision": "4315941b2b6170ad2d09",
+    "url": "/static/js/3.e56ccadf.chunk.js"
+  },
+  {
+    "revision": "bdd15377e0a952d4f696",
+    "url": "/static/js/4.e93459f5.chunk.js"
+  },
+  {
+    "revision": "75e7f9091437f3a7fe07",
+    "url": "/static/js/5.86ba5885.chunk.js"
+  },
+  {
+    "revision": "0b3641182c6e72eee7fb",
+    "url": "/static/js/main.5ac3607c.chunk.js"
+  },
+  {
+    "revision": "8379627754728c5fecf5",
+    "url": "/static/js/runtime~main.9457b506.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
